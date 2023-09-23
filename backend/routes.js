3b9fbde3aeebe03c5eb55fa9a90f81b0ec72b3ce
Cherry Machado <136278110+Cherry-2023@users.noreply.github.com>
2023-09-23 15:14:35 -0300

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const UserController = require('./app/controllers/UserController');
+const UserController = require('./app/controllers/UserController.js');
 
 const router = new express.Router();
 
